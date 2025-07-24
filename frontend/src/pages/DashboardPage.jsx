671eb497102d30aa469e3b6d965dcc2cd1faae15
Md. Tanveer Ahmed <tanveera2001@gmail.com>
2025-07-25 00:29:51 +0600

    Dashboard done

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -3,11 +3,9 @@ import { useAuthStore } from "../store/authStore";
 import { formatDate } from "../utils/date";
 
 const DashboardPage = () => {
-	const { user, logout } = useAuthStore();
+	const { user} = useAuthStore();
 
-	const handleLogout = () => {
-		logout();
-	};
+
 	return (
 		<motion.div
 			initial={{ opacity: 0, scale: 0.9 }}
@@ -53,24 +51,6 @@ const DashboardPage = () => {
 					</p>
 				</motion.div>
 			</div>
-
-			<motion.div
-				initial={{ opacity: 0, y: 20 }}
-				animate={{ opacity: 1, y: 0 }}
-				transition={{ delay: 0.6 }}
-				className='mt-4'
-			>
-				<motion.button
-					whileHover={{ scale: 1.05 }}
-					whileTap={{ scale: 0.95 }}
-					onClick={handleLogout}
-					className='w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
-				font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
-				 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900'
-				>
-					Logout
-				</motion.button>
-			</motion.div>
 		</motion.div>
 	);
 };
